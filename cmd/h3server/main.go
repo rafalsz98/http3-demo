0bfd021e22cda3343665b537c1c308daba7bde44
Rafal Szczepanik <rafal.sz@tutanota.com>
2023-10-22 16:41:29 +0200

    Remove old h3 implementation

--- cmd/h3server/main.go
+++ cmd/h3server/main.go
@@ -1,90 +1,13 @@
 package main
 
 import (
-	"crypto/md5"
 	"fmt"
-	"io"
 	"log"
-	"mime/multipart"
 	"net/http"
 
-	"github.com/rafalsz98/http3-demo/utils"
-
 	"github.com/quic-go/quic-go/http3"
+	"github.com/rafalsz98/http3-demo/utils"
 )
-
-type Size interface {
-	Size() int64
-}
-
-func setupHandler(www string) http.Handler {
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("TEST"))
-	})
-
-	mux.HandleFunc("/demo/tile", func(w http.ResponseWriter, r *http.Request) {
-		// Small 40x40 png
-		w.Write([]byte{
-			0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a, 0x00, 0x00, 0x00, 0x0d,
-			0x49, 0x48, 0x44, 0x52, 0x00, 0x00, 0x00, 0x28, 0x00, 0x00, 0x00, 0x28,
-			0x01, 0x03, 0x00, 0x00, 0x00, 0xb6, 0x30, 0x2a, 0x2e, 0x00, 0x00, 0x00,
-			0x03, 0x50, 0x4c, 0x54, 0x45, 0x5a, 0xc3, 0x5a, 0xad, 0x38, 0xaa, 0xdb,
-			0x00, 0x00, 0x00, 0x0b, 0x49, 0x44, 0x41, 0x54, 0x78, 0x01, 0x63, 0x18,
-			0x61, 0x00, 0x00, 0x00, 0xf0, 0x00, 0x01, 0xe2, 0xb8, 0x75, 0x22, 0x00,
-			0x00, 0x00, 0x00, 0x49, 0x45, 0x4e, 0x44, 0xae, 0x42, 0x60, 0x82,
-		})
-	})
-
-	mux.HandleFunc("/demo/tiles", func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "<html><head><style>img{width:40px;height:40px;}</style></head><body>")
-		for i := 0; i < 200; i++ {
-			fmt.Fprintf(w, `<img src="/demo/tile?cachebust=%d">`, i)
-		}
-		io.WriteString(w, "</body></html>")
-	})
-
-	mux.HandleFunc("/demo/echo", func(w http.ResponseWriter, r *http.Request) {
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			fmt.Printf("error reading body while handling /echo: %s\n", err.Error())
-		}
-		w.Write(body)
-	})
-
-	// accept file uploads and return the MD5 of the uploaded file
-	// maximum accepted file size is 1 GB
-	mux.HandleFunc("/demo/upload", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodPost {
-			err := r.ParseMultipartForm(1 << 30) // 1 GB
-			if err == nil {
-				var file multipart.File
-				file, _, err = r.FormFile("uploadfile")
-				if err == nil {
-					var size int64
-					if sizeInterface, ok := file.(Size); ok {
-						size = sizeInterface.Size()
-						b := make([]byte, size)
-						file.Read(b)
-						md5 := md5.Sum(b)
-						fmt.Fprintf(w, "%x", md5)
-						return
-					}
-					// err = errors.New("couldn't get uploaded file size")
-				}
-			}
-			// fmt.Printf(err.Error())
-			// utils.DefaultLogger.Infof("Error receiving upload: %#v", err)
-		}
-		io.WriteString(w, `<html><body><form action="/demo/upload" method="post" enctype="multipart/form-data">
-				<input type="file" name="uploadfile"><br>
-				<input type="submit">
-			</form></body></html>`)
-	})
-
-	return mux
-}
 
 var envs utils.Environment
 
@@ -92,9 +15,13 @@ func main() {
 	envs = utils.GetEnvs()
 
 	address := fmt.Sprintf("0.0.0.0:%s", envs.Port)
-	log.Printf("Starting server on %s", address)
+	log.Printf("Starting HTTP/3 server on %s", address)
 
-	err := http3.ListenAndServe(address, envs.SSL.CertPath, envs.SSL.KeyPath, setupHandler(""))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("TEST"))
+	})
+
+	err := http3.ListenAndServe(address, envs.SSL.CertPath, envs.SSL.KeyPath, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
