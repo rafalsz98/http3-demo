ed8e2e9f40f473d82b2a5acf67a54b2900a975a5
Rafal Szczepanik <rafal.sz@tutanota.com>
2023-10-22 17:20:45 +0200

    Add pure H2 golang server

--- cmd/h2server/main.go
+++ cmd/h2server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/rafalsz98/http3-demo/utils"
+)
+
+var envs utils.Environment
 
 func main() {
-	fmt.Print("Hello world")
+	envs = utils.GetEnvs()
+
+	address := fmt.Sprintf("0.0.0.0:%s", envs.Port)
+	log.Printf("Starting HTTP/2 server on %s", address)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("TEST"))
+	})
+
+	err := http.ListenAndServeTLS(address, envs.SSL.CertPath, envs.SSL.KeyPath, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
