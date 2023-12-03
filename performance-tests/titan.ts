ad1b243424de68dfba4101f7c1bb3b9e74fc5979
Rafal Szczepanik <rafal.sz@tutanota.com>
2023-12-03 12:52:10 +0100

    Add POST endpoint, add titan perftest

---
+++ performance-tests/titan.ts
@@ -0,0 +1,67 @@
+import { readableStreamToText, spawn } from "bun";
+import { program } from "commander";
+import { exit } from "process";
+import { METRICS } from "./common";
+
+program
+  .version("1.0.0", "-v, --version")
+  .description("Benchmark testing for file upload endpoint")
+  .usage("[OPTIONS]...")
+  .argument("<ipAddress>", "IP address of the server to benchmark")
+  .option("-h3, --http3", "Use HTTP/3 protocol")
+  .option("-n <integer>", "Number of test runs, defaults to 1", "1")
+  .option(
+    "-o, --output <path>",
+    "Path to write output to, defaults to - (stdin)",
+    "-"
+  )
+  .option("-f, --file <path>", "Path of the file to upload");
+
+program.parse();
+const { ipAddress, http3, n, output, file } = program.opts() as {
+  ipAddress: string;
+  http3: boolean;
+  n: string;
+  output: string;
+  file: string;
+};
+
+const writeFormat = METRICS.map((field) => `%{${field}}`).join(";");
+
+const getCurlSpawn = (iteration: number) => [
+  "curl",
+  "-X POST",
+  "-s",
+  "-o /dev/null",
+  `-w "${writeFormat}"`,
+  "--form",
+  `file=@${file}`,
+  ...(http3 ? ["--http3"] : []),
+  `${ipAddress}/upload`,
+];
+
+const columns = METRICS.join(";");
+const results: string[] = [columns];
+
+const fileWriter = output === "-" ? undefined : Bun.file(output).writer();
+fileWriter?.write(columns + "\n");
+
+for (let i = 0; i < Number(n); i++) {
+  const { stdout, exited } = spawn(getCurlSpawn(i));
+  await exited;
+  const output = (await readableStreamToText(stdout)).replaceAll('"', "");
+
+  fileWriter?.write(output + "\n");
+  results.push(output);
+
+  process.stdout.write(`${((i + 1) / Number(n)) * 100}%`);
+}
+
+const dataToSave = results.join("\n");
+
+if (output === "-") {
+  console.log(dataToSave);
+}
+
+fileWriter?.end();
+exit(0);
--- server/utils/httpSetup.go
+++ server/utils/httpSetup.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 )
@@ -16,5 +17,50 @@ func SetupHttp() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("TEST"))
+	})
+
+	http.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			w.Write([]byte("405 - Method Not Allowed"))
+			return
+		}
+
+		// Parse the multipart form in the request
+		err := r.ParseMultipartForm(32 << 20) // 32MB is the default used by http package
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("500 - Internal Server Error (ParseMultipartForm)"))
+			return
+		}
+
+		// Get the file from the uploaded form
+		file, _, err := r.FormFile("file")
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("400 - Bad Request"))
+			return
+		}
+		defer file.Close()
+
+		// Calculate the size of the file
+		fileSize, err := file.Seek(0, 2) // Seek to the end of the file
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("500 - Internal Server Error (Seek)"))
+			return
+		}
+
+		// Set the file pointer back to the beginning
+		_, err = file.Seek(0, 0)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("500 - Internal Server Error"))
+			return
+		}
+
+		// Return the size of the file to the client
+		fmt.Fprintf(w, "File size: %d bytes", fileSize)
+		w.WriteHeader(http.StatusOK)
 	})
 }
